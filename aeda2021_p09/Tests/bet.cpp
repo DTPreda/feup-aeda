7289bfbe338ec9d20b4cf361aed486ceafe8cf02
David Preda <up201904726@fe.up.pt>
2020-12-06 17:23:25 +0000

    Added p09 and p10

---
+++ aeda2021_p09/Tests/bet.cpp
@@ -0,0 +1,23 @@
+#include "bet.h"
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+
+//TODO
+bool Bet::contains(unsigned num) const
+{
+	return true;
+}
+
+//TODO
+void Bet::generateBet(const vector<unsigned>& values, unsigned n)
+{
+
+}
+
+//TODO
+unsigned Bet::countRights(const tabHInt& draw) const
+{
+	return 0;
+}
---
+++ aeda2021_p09/Tests/player.cpp
@@ -0,0 +1,20 @@
+#include "player.h"
+
+//TODO
+void Player::addBet(const Bet& b)
+{
+
+}
+
+//TODO
+unsigned Player::betsInNumber(unsigned num) const
+{
+	return 0;
+}
+
+//TODO
+tabHBet Player::drawnBets(const tabHInt& draw) const
+{
+	tabHBet res;
+	return res;
+}
